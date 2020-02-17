ba5f4bf211e4136e5e21be0abb8f5bdab53fa2cb
San <caothesan@gmail.com>
2020-02-17 20:20:59 +0700

    fix bug

--- run.sh
+++ run.sh
@@ -41,8 +41,8 @@ fi
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install gcc-4.9 g++-4.9
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+sudo apt-get install gcc g++
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 
 # Install ninja linux
 wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
